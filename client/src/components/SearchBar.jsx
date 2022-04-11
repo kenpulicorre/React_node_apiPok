892fe1ea2049c5db85a55a7e839531d3fede2a84
kenneth <ingkeneidel@gmail.com>
2022-04-10 23:40:44 -0500

    modifique el search con el enter

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -9,18 +9,29 @@ export default function SearchBar() {
   const [name, setName] = useState("");
 
   function handleInputOnChange(e) {
+    console.log("handleInputOnChange--on--SearchBar.jsx! ");
     e.preventDefault();
     setName(e.target.value.toLowerCase(), console.log(name));
     // console.log(name);
   }
 
   function handleSubmit(e) {
+    console.log("handleSubmit--on--SearchBar.jsx! ");
     e.preventDefault();
     if (name) {
       dispatch(getNamePokemons(name));
       setName("");
     }
   }
+  let handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      console.log("handleKeyPress--on--SearchBar.jsx! ");
+      if (name) {
+        dispatch(getNamePokemons(name));
+        setName("");
+      }
+    }
+  };
 
   return (
     <div>
@@ -29,6 +40,7 @@ export default function SearchBar() {
         placeholder="pokemon a buscar..."
         onChange={(e) => handleInputOnChange(e)}
         value={name}
+        onKeyPress={(e) => handleKeyPress(e)}
       />
       <button type="submit" onClick={(e) => handleSubmit(e)}>
         Buscar
